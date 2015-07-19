8f75380b59d73dc0fcf40121cc6b96470416c28b
Laspimon <casperlehmann@gmail.com>
2015-07-19 13:03:06 +0200

    Making changes. Problem not solved.

--- transmute/transmute.py
+++ transmute/transmute.py
@@ -55,8 +55,10 @@ class TranslationModeller(object):
         return overview, navi
 
     def get_dict(self, container, overview, navi, level):
-        cvals = container.values()
-        if not False in [type(x) in INDEVISIBLE_FORMS for x in cvals]:
+        for x in [container, overview, navi, level]:
+            print ('+=+=+=', x)
+        print ()
+        if False: #not False in [type(x) in INDEVISIBLE_FORMS for x in container.values()]:
             if self.debug: self.printout(container, overview, navi, level, 0, 0, 'dict_0')
             overview.append(navi[:] + [[dict, container]])
         else:
@@ -119,7 +121,7 @@ class TranslationModeller(object):
         print()
         print('____________________________', msg)
         print (container)
-        print (key, '\t\t', val)    
+        print (key, '\t\t', val)
         #print ('navi ', navi)
         print ('lvl  ', level)
         print('navi:')
@@ -151,6 +153,7 @@ class Transmute(object):
             pass
 
     def follow_roadmap(self, road, output):
+        print ('""""""""""""" follow road:', road)
         next_level = output
         for step in road[:-1]:
             if self.debug: print (road)
@@ -169,15 +172,28 @@ class Transmute(object):
         return next_level
 
     def finalize(self, items = None, next_level = None):
+        print()
+        print ('[--- items:')
+        for x in items: print ('[---', x)
+        print ('[---')
+        print ('[--- next_level:')
+        for x in next_level: print ('[---', x)
+
+
         if len(items) == 2:
             if items[0] is dict:
+                print()
                 print ('items', type(items))
                 print (items)
-                print ('next_level', type(next_level))
-                print (next_level)
                 print()
+                print ('next_level', type(next_level))
+                print ('|||||||', next_level)
+                print()
+                next_level.append(items[1])
                 for k,v in items[1].items():
-                    next_level[k] = v
+                    print ('k', k)
+                    print ('v', v)
+                    #next_level[k] = v
             elif items[0] is list:
                 for x in items[1]:
                     next_level.append(x)
@@ -207,6 +223,10 @@ class Transmute(object):
         print ('----------------------')
         print (output)
         print ('----------------------')
+        for x in overview:
+            print('---', ' '.join([str(len(y)) for y in x]))
+        for x in overview:
+            print('---', x)
         for road in overview:
             next_level = self.follow_roadmap(road = road, output = output)
             self.finalize(items = road[-1], next_level = next_level)
@@ -215,16 +235,16 @@ class Transmute(object):
     def test_it(self, l_tree, r_tree):
         #print(input_tree)
         l_recreated = self.recreate(self.l_overview)
-        if l_recreated == l_tree: 
+        if l_recreated == l_tree:
             print (True)
-        else: 
+        else:
             print (False, l_recreated)
         print(4 * '========')
         
         r_recreated = self.recreate(self.r_overview)
-        if r_recreated == r_tree: 
+        if r_recreated == r_tree:
             print (True)
-        else: 
+        else:
             print (False)
             print ('|', r_tree)
             print ('|', r_recreated)
@@ -233,30 +253,42 @@ class Transmute(object):
         print()
 
     def open_dict_container(self, next_level, container_type, container_info, child_type, child_info, index):
+        print (20*'=====')
         for i,x in enumerate([next_level, container_type, container_info, child_type, child_info, index]):
             print (i, x)
+        print()
+        container_keys = container_info
+        child_len = child_info
+        child_key = index
+        if len(next_level) == 0:
+            for key in container_keys:
+                if not key in next_level:
+                    next_level[key] = None
         if child_type is dict:
             print (1)
-            child_key = index
-            if not child_key in next_level.keys():
+            if next_level[child_key] is None:
                 next_level[child_key] = child_type()
             next_level = next_level[child_key]
         elif child_type is list:
             print (2)
-            container_keys = container_info
-            child_len = child_info
-            child_index = index
-            if len(next_level) == 0:
-                for key in container_keys:
-                    if not key in next_level:
-                        next_level[key] = None #child_type() 
-                        # This is wrong. We only know single child type here. Cannot extrapolate.
-                        # Throw this up in the beginning of the method so that sub trees are created once and only once.
-                        if key == child_type:
-                            next_level[child_index] = child_type()
-            if not child_index in next_level:
-                next_level[child_index] = child_type()
-            next_level = next_level[child_index]
+            #child_index = index
+            #container_keys = container_info
+            #child_len = child_info
+            #child_index = index
+            #if len(next_level) == 0:
+            #    for key in container_keys:
+            #        if not key in next_level:
+            #            next_level[key] = None #child_type() 
+            #            # This is wrong. We only know single child type here. Cannot extrapolate.
+            #            # Throw this up in the beginning of the method so that sub trees are created once and only once.
+            #            if key == child_type:
+            #                next_level[child_index] = child_type()
+            if next_level[child_key] is None:
+                next_level[child_key] = child_type()
+            print(child_type)
+            if not child_key in next_level:
+                next_level[child_key] = child_type()
+            next_level = next_level[child_key]
         elif child_type is str or child_type is unicode:
             print (3)
             # This shouldn't happen.
@@ -266,12 +298,15 @@ class Transmute(object):
             next_level[index] = ''
         else:
             print('___________fail 1')
+        print()
         for i,x in enumerate([next_level, container_type, container_info, child_type, child_info, index]):
             print (i, x)
         print ('{2--<', 'next_level', type(next_level))
+        print (20*'=====')
         return next_level
 
     def open_list_container(self, next_level, container_type, container_info, child_type, child_info, index, output = None):
+        print (20*':::::')
         self.printout_list(next_level, container_type, container_info, child_type, child_info, index, output, 'Open List')
         if child_type is list:
             container_len = container_info
@@ -296,6 +331,7 @@ class Transmute(object):
             #1/0
         else:
             print('___________fail 2', child_type)
+        print (20*':::::')
         return next_level
 
     def printout_list(self, next_level, container_type, container_info, child_type, child_info, index, output, msg):
@@ -319,6 +355,21 @@ class Transmute(object):
 print ('left\n', left_form)
 print ('right\n', right_form)
 print()
+
+
+left_form = {
+    #'key': 'abc123',
+    #'name': {'first': 'joe', 'last': 'stein'},
+    #'triples': {1: 3, 2: 6, 3: 9, 4: 12, 5: 15},
+    'opposites': [('low','high'),('big','small')]
+    }
+right_form = {
+    #'uuid': 'abc123',
+    #'first_name': 'joe',
+    #'last_name': 'stein',
+    #'triple_tuples': [(1, 3), (2, 6), (3, 9), (4, 12), (5, 15)],
+    'opposites': [{'first': 'low', 'second': 'high'}, {'first': 'big', 'second': 'small'}]
+    }
 
 example = Transmute(left_form, right_form, False)
 example.test_it(left_form, right_form)
