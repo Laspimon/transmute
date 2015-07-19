dfadef4c1df9a4cd89684e06b00c52654e5d90a3
Laspimon <casperlehmann@gmail.com>
2015-07-19 21:12:02 +0200

    Closing in.

--- transmute/transmute.py
+++ transmute/transmute.py
@@ -159,45 +159,25 @@ class Transmute(object):
         for step in road:#[:-1]:
             if self.debug: print (road)
             #print ('== step:')
-            #print (step)
+            print (step)
             container_type, container_info, child_type, child_info, index = step
+            this_level = next_level
+            next_type = child_type
             if container_type is dict:
                 next_level = self.open_dict_container(next_level, container_type,
                             container_info, child_type, child_info, index)
             elif container_type is list or container_type is tuple:
                 next_level = self.open_list_container(next_level, container_type,
                             container_info, child_type, child_info, index, output)
-                if container_type is tuple:
-                    #print ('!!!!!!!!!!!!!!!!')
-                    next_level = tuple(next_level)
+            #!! if next_type is tuple:
+            #!!     this_level[index] = tuple(this_level[index])
         #print ('{1--<', 'next_level', type(next_level))
-        #print (next_level)
-        #print (road)
         return next_level
 
     def finalize(self, items = None, next_level = None):
-        #print()
-        #print ('[--- items:')
-        #for x in items: print ('[---', x)
-        #print ('[---')
-        #print ('[--- next_level:')
-        #for x in next_level: print ('[---', x)
-
-
         if len(items) == 2:
             if items[0] is dict:
-                #print()
-                #print ('items', type(items))
-                #print (items)
-                #print()
-                #print ('next_level', type(next_level))
-                #print ('|||||||', next_level)
-                #print()
                 next_level.append(items[1])
-                #for k,v in items[1].items():
-                #    print ('k', k)
-                #    print ('v', v)
-                #    #next_level[k] = v
             elif items[0] is list:
                 for x in items[1]:
                     next_level.append(x)
@@ -205,7 +185,6 @@ class Transmute(object):
                 next_level.append(items[1])
             elif items[0] is str:
                 next_level = items[1]
-                #for i, x in enumerate(items): print (i, '+', x)
         else:
             if items[3] is dict:
                 for k,v in items[-1].items():
@@ -219,15 +198,9 @@ class Transmute(object):
                 next_level[items[-2]] = items[-1]
 
     def recreate(self, overview, output = None):
-        #for x in overview: print (':', x)
         if output is None: output = {}
-        #for x in overview:
-        #    print('---', ' '.join([str(len(y)) for y in x]))
-        #for x in overview:
-        #    print('---', x)
         for road in overview:
             next_level = self.follow_roadmap(road = road, output = output)
-            #self.finalize(items = road[-1], next_level = next_level)
         return output
 
     def test_it(self, l_tree, r_tree):
@@ -253,17 +226,10 @@ class Transmute(object):
         #print()
 
     def open_dict_container(self, next_level, container_type, container_info, child_type, child_info, index):
-        #print ('???')
-        #print (20*'=====')
-        #for x in container_info: 
-        #    if not x in next_level:
-        #        next_level = None
-        #for i,x in enumerate([next_level, container_type, container_info, child_type, child_info, index]):
-        #    print ('__--__--__--__--', i, x)
-        #print()
         container_keys = container_info
         child_len = child_info
         child_key = index
+        print (next_level)
         if len(next_level) == 0:
             for key in container_keys:
                 if not key in next_level:
@@ -275,71 +241,49 @@ class Transmute(object):
         elif child_type is list:
             if next_level[child_key] is None:
                 next_level[child_key] = child_type()
-            #print(child_type)
             if not child_key in next_level:
                 next_level[child_key] = child_type()
             next_level = next_level[child_key]
         elif child_type is str or child_type is unicode:
-            #print('___', child_type, index, next_level[index])
-            #next_level[index] = ''
-            #print ('| Container: list, Child: str')
-            #print ('index:', index)
             next_level[index] = child_info
         else:
             print('___________fail 1')
-        #print()
-        #for i,x in enumerate([next_level, container_type, container_info, child_type, child_info, index]):
-        #    print (i, x)
         #print ('{2--<', 'next_level', type(next_level))
         #print (20*'=====')
+        print (next_level)
+        print()
         return next_level
 
     def open_list_container(self, next_level, container_type, container_info, child_type, child_info, index, output = None):
-        print ('======')
-        print (output)
+        #print ('======')
+        #print (output)
+        #print (next_level)
         for x in range(container_info-len(next_level)):
             next_level.append(None)
         #print (20*':::::')
         self.printout_list(next_level, container_type, container_info, child_type, child_info, index, output, 'Open List')
         if child_type is list or child_type is tuple:
-            container_len = container_info
-            child_index = index
-            print (output)
+            #print (output)
             if len(next_level) == 0:
                 for x in range(container_info):
                     next_level.append(None)
-                next_level = tuple(next_level)
             elif not container_info == len(next_level):
                 print ('container_info != len(next_level)')
                 for x in range(container_info-len(next_level)):
                     next_level.append(None)
-            if next_level[child_index] is None:
-                print('NEJ')
-                next_level[child_index] = [] #!! child_type()
-            next_level = next_level[child_index]
-            print (output)
+            if next_level[index] is None:
+                next_level[index] = [] #!! child_type()                
+            next_level = next_level[index]
+            #print (output)
         elif child_type is str or child_type is unicode:
-            #print ('| Container: list, Child: str') #!!
-            #print ('index:', index)
             next_level[index] = child_info
-            #next_level[index] = 999
-        elif child_type is tuple:
-            pass#container_info = container_info
         elif child_type is dict:
-            #print ()
             next_level[index] = child_type()
             next_level = next_level[index]
-            #for x in range(container_info-len(next_level)):
-            #    next_level.append(None)
-            #for x in [next_level, container_type, container_info, child_type, child_info, index, output]:
-            #    print (x)
-            #print()
-            #print ( container_info , child_info)
-            #print()
-            #1/0
         else:
             print('___________fail 2', child_type)
         #print (20*':::::')
+        self.printout_list(next_level, container_type, container_info, child_type, child_info, index, output, 'Open List')
         return next_level
 
     def printout_list(self, next_level, container_type, container_info, child_type, child_info, index, output, msg):
@@ -364,22 +308,27 @@ print ('left\n', left_form)
 print ('right\n', right_form)
 print()
 
-
-left_form = {
-    #'key': 'abc123',
-    #'name': {'first': 'joe', 'last': 'stein'},
-    #'triples': {1: 3, 2: 6, 3: 9, 4: 12, 5: 15},
-    'opposites': [('low','high'),('big','small')]
-    }
-right_form = {
-    #'uuid': 'abc123',
-    #'first_name': 'joe',
-    #'last_name': 'stein',
-    #'triple_tuples': [(1, 3), (2, 6), (3, 9), (4, 12), (5, 15)],
-    'opposites': [{'first': 'low', 'second': 'high'}, {'first': 'big', 'second': 'small'}]
-    }
+left_form = {'opposites': [('low','high'),('big','small')]}
+right_form = {'opposites': [{'first': 'low', 'second': 'high'}, {'first': 'big', 'second': 'small'}]}
 
 example = Transmute(left_form, right_form, False)
 example.test_it(left_form, right_form)
+
+left_form = {
+    'key': 'abc123',
+    'name': {'first': 'joe', 'last': 'stein'},
+    'triples': {1: 3, 2: 6, 3: 9, 4: 12, 5: 15},
+    'opposites': [('low','high'),('big','small')]
+    }
+right_form = {
+    'uuid': 'abc123',
+    'first_name': 'joe',
+    'last_name': 'stein',
+    'triple_tuples': [(1, 3), (2, 6), (3, 9), (4, 12), (5, 15)],
+    'opposites': [{'first': 'low', 'second': 'high'}, {'first': 'big', 'second': 'small'}]
+    }
+
+#example = Transmute(left_form, right_form, False)
+#example.test_it(left_form, right_form)
 
 #print (left_form)
